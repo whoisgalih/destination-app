b4cc98ac9d1c141ac7b3fe9f9d0dd4cf3eae44c5
Galih Akbar <galihakbar.ga91@gmail.com>
2022-04-26 15:01:54 +0700

    add button on home page

--- src/scripts/pages/Home.js
+++ src/scripts/pages/Home.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import PageTitle from '../components/PageTitle';
+import { Link } from 'react-router-dom';
+
+import '../../styles/components/ButtonArea.css';
 
 class Home extends Component {
   render() {
     return (
       <div className='container-xxl custom-padding'>
         {/* prettier-ignore */}
-        <PageTitle 
+        <PageTitle
           sectionName='whaaattt??????' 
           title='Destination App' 
           subtitle='Place to explore destinations in the world'
-        />
+        > 
+          <div className="button-area">
+            <Link className="button" to='/destinations'>
+              <div className="text-primary">
+                Go to Destination Page
+              </div>
+            </Link>
+            <Link className="button" to='/about-us'>
+              <div className="text-primary">
+               Know More About Us
+              </div>
+            </Link>
+          </div>
+        </PageTitle>
       </div>
     );
   }
